404bb3e54062e7b4d459682086350ee8aa60ad5e
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-06-19 20:54:01 +0300

    logo-fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,12 +38,12 @@ export default function Navbar() {
                 <div className="flex-shrink-0 flex items-center">
                   <img
                     className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                    src="/logo-dark.svg"
                     alt="Workflow"
                   />
                   <img
                     className="hidden lg:block h-8 w-auto"
-                    src="/rzlogo.svg"
+                    src="/logo-text-dark.svg"
                     alt="Workflow"
                   />
                 </div>
