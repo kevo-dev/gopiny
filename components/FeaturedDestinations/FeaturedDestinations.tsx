3f99d62a1312f8c96163388a5b058b14f5eb053b
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-06-19 09:57:44 +0300

    featured destination

--- components/FeaturedDestinations/FeaturedDestinations.tsx
+++ components/FeaturedDestinations/FeaturedDestinations.tsx
@@ -5,22 +5,22 @@ const destinations = [
   {
     name: 'Raja Ampat',
     country: 'Indonesia',
-    image: '/assets/images/destination-1.jpg',
+    image: '/images/destination-1.jpg',
   },
   {
     name: 'Fanjingshan',
     country: 'China',
-    image: 'assets/images/destination-2.jpg',
+    image: '/images/destination-2.jpg',
   },
   {
     name: 'Vevey',
     country: 'Switzerland',
-    image: '/assets/images/destination-3.jpg',
+    image: '/images/destination-3.jpg',
   },
   {
     name: 'Skadar',
     country: 'Montegero',
-    image: '/assets/images/destination-4.jpg',
+    image: '/images/destination-4.jpg',
   },
 ];
 
@@ -31,8 +31,8 @@ const FeaturedDestinations = () => (
       {destinations.map(({ name, country, image }, index) => (
         <div className="desination-wrapper p-3 mt-1 mr-auto" key={index}>
           <div className="destination relative w-60 h-80 border-2 rounded-md cursor-pointer bg-slate-800">
-            <img src={image} alt={name} />
-            <div className="content absolute bottom-0 h-5 min-w-10">
+            <img src={image} alt={name} className="absolute object-fit" />
+            <div className="content absolute bottom-0 h-12 min-w-full">
               <p className="text-xl text-gray-800 font-semibold">{name}</p>
               <p className="text-lg text-gray-800">{country}</p>
             </div>
