69707f35c2b6487777094c13cd94551373dff60b
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-06-20 13:09:13 +0300

    favicon

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -9,13 +9,13 @@ const regions = ['Region 1', 'Region 2', 'Region 3'];
 
 const Banner = () => (
   <div className="flex items-center h-full mt-0">
-    <div className="relative w-1/2">
-      <p className="text-4xl font-bold text-purple-800 mt-10 ml-5 mb-20 font-mono lg:mt-10 lg:mr-0 ">Explore Kisumu</p>
-      <p className="text-lg text-purple-800 ml-4">Package finder</p>
+    <div className="relative w-1/2 mr-0">
+      <p className="text-3xl font-bold text-purple-800 my-10 ml-5 font-mono lg:mt-10 lg:mr-0 ">Explore Kisumu</p>
+      <p className="text-md text-purple-800 ml-4">Package finder</p>
       <div className="w-8 h-1 bg-purple-800 mt-4 mr-0 ml-6" />
       <div className="flex mt-6 mr-0">
         <div className="flex ">
-          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-none">
+          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-black">
             <option>Location</option>
             {locations.map((location, index) => (
               <option value={location} key={index}>
@@ -23,7 +23,7 @@ const Banner = () => (
               </option>
             ))}
           </select>
-          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-none">
+          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-black">
             <option>Activity</option>
             {activities.map((activity, index) => (
               <option value={activity} key={index}>
@@ -32,24 +32,7 @@ const Banner = () => (
             ))}
           </select>
         </div>
-        <div className="row">
-          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-none">
-            <option>Grade</option>
-            {grades.map((grade, index) => (
-              <option value={grade} key={index}>
-                {grade}
-              </option>
-            ))}
-          </select>
-          <select className="w-40 h-11 border-2 m-2 pt-0 pr-3 rounded-lg cursor-pointer mr-5 text-md focus:outline-none">
-            <option>Regions</option>
-            {regions.map((region, index) => (
-              <option value={region} key={index}>
-                {region}
-              </option>
-            ))}
-          </select>
-        </div>
+
       </div>
       <button className="bg-orange-500 h-10 w-30 p-2 m-4 border-2 rounded-lg">Explore</button>
     </div>
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -14,7 +14,7 @@ interface CardProps {
 const Card = ({ title, subtitle, image, buttonLabel, reversed }: CardProps) => {
   return (
     <div
-      className="card relative flex items-center px-auto mx-auto"
+      className="card relative flex items-center mt-12 mr-0"
       style={{ flexDirection: reversed ? 'row-reverse' : 'row' }}
     >
       {reversed ? (
@@ -26,18 +26,19 @@ const Card = ({ title, subtitle, image, buttonLabel, reversed }: CardProps) => {
 
         </>
       )}
+      
       <Image
-    width={500}
-    height={500} 
-    className="object-cover"
+    width={600}
+    height={600} 
+    className="h-full w-1/2 object-cover mr-auto sm:hidden "
     src={image} alt={title} />
 
       <div className="w-1/2">
-        <p className="text-2xl font-semibold">{title}</p>
-        <p className="text-xl mt-1 mr-0 mb-3 ml-0 w-9/12 text-gray-800">
+        <p className="text-xl font-semibold p-4">{title}</p>
+        <p className="text-md mt-2 mr-0 mb-4 ml-0 w-9/12 text-gray-800 lg:w-8/10 ">
           {subtitle}
         </p>
-        <button className="bg-orange-500 h-10 w-30 p-2">{buttonLabel}</button>
+        <button className="bg-orange-700 h-10 w-30 p-2">{buttonLabel}</button>
       </div>
     </div>
   );
