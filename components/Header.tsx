ebf6534176bc40a11450612fb5d252ae22878cf3
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-07 11:57:02 +0300

    modified:   components/Header.tsx 	new file:   public/product.json

--- components/Header.tsx
+++ components/Header.tsx
@@ -127,7 +127,6 @@ export default function Example() {
                               href={item.href}
                               className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
                             >
-                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
                               <div className="ml-4">
                                 <p className="text-base font-medium text-gray-900">{item.name}</p>
                                 <p className="mt-1 text-sm text-gray-500">{item.description}</p>
