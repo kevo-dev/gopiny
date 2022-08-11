510125c86f034c440b91e749adb1aeffe32509be
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-11 19:18:21 +0300

    modified:   components/ProductOverview.tsx 	new file:   pages/tour1.tsx 	deleted:    pages/tour_details/tour1.tsx

--- components/ProductOverview.tsx
+++ components/ProductOverview.tsx
@@ -149,13 +149,13 @@ const TourDetails = (props:ITourDetailsProps) => {
         {/* Product info */}
         <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
           <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{product.name}</h1>
+            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{props.name}</h1>
           </div>
 
           {/* Options */}
           <div className="mt-4 lg:mt-0 lg:row-span-3">
             <h2 className="sr-only">Product information</h2>
-            <p className="text-3xl text-gray-900">{product.price}</p>
+            <p className="text-3xl text-gray-900">{props.price}</p>
 
             {/* Reviews */}
             <div className="mt-6">
@@ -294,7 +294,7 @@ const TourDetails = (props:ITourDetailsProps) => {
               <h3 className="sr-only">Description</h3>
 
               <div className="space-y-6">
-                <p className="text-base text-gray-900">{product.description}</p>
+                <p className="text-base text-gray-900">{props.description}</p>
               </div>
             </div>
 
