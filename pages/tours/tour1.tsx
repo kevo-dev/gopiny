466564ef4efd2022d4a16d414a27d3e8f48ce8cd
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-11 21:49:29 +0300

    renamed:    pages/tour1.tsx -> pages/tours/tour1.tsx 	new file:   pages/tours/tour2.tsx 	new file:   pages/tours/tour3.tsx 	new file:   pages/tours/tour4.tsx 	new file:   pages/tours/tour5.tsx 	new file:   pages/tours/tour6.tsx 	new file:   pages/tours/tour7.tsx 	new file:   pages/tours/tour8.tsx

--- pages/tours/tour1.tsx
+++ pages/tours/tour1.tsx
@@ -1,12 +1,12 @@
 
-import { Base }  from '/components/layout/Base'
-import { Section }  from '../components/layout/Section'
+import { Base }  from '../../components/layout/Base'
+import { Section }  from '../../components/layout/Section'
 
-import { AppConfig } from '../utils/AppConfig';
-import { Tours } from '../components/Tours';
-import ProductList from '../components/ProductList';
-import { TourDetails } from '../components/ProductOverview';
-import ProductFeatures from '../components/ProductFeatures';
+import { AppConfig } from '../../utils/AppConfig';
+import { Tours } from '../../components/Tours';
+import ProductList from '../../components/ProductList';
+import { TourDetails } from '../../components/ProductOverview';
+import ProductFeatures from '../../components/ProductFeatures';
 
 const TourDetail = () => {
     return(
