6da1f11c75a877f3b91618b910d56a0705e474fd
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-14 19:29:02 +0300

    modified:   components/ReviewScore.tsx

--- components/ReviewScore.tsx
+++ components/ReviewScore.tsx
@@ -9,7 +9,7 @@ const ReviewScore = (props: IContactProps) => {
   return (
     <>
       <Section>
-        <div className="relative z-10 rounded-md shadow-md p-4 md:p-10 lg:p-20 max-w-6xl mx-auto mb-20 -mt-4">
+        <div className="relative z-10 rounded-md shadow-md p-4 md:p-10 lg:p-20 max-w-3xl mx-auto mb-20 -mt-4">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
             <div className="mb-8 mx-auto ">
               <h1 className="text-white font-bold text-9xl py-24">7.0</h1>{" "}
@@ -21,54 +21,67 @@ const ReviewScore = (props: IContactProps) => {
                   Services
                 </span>
                 <span className="text-sm font-medium text-blue-700 dark:text-white">
-                  45%
+                  80%
                 </span>
               </div>
-              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-8 ">
-                <div className="bg-blue-600 h-2.5 rounded-full w-1/2"></div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-2 ">
+                <div className="bg-blue-600 h-2.5 rounded-full w-9/12"></div>
               </div>
-
 
               <div className="flex justify-between">
-                <span className="text-base font-medium text-blue-700 dark:text-white mb-4">
-                  Services
+                <span className="text-base font-medium text-blue-700 dark:text-white mb-2">
+                  Locations
                 </span>
                 <span className="text-sm font-medium text-blue-700 dark:text-white">
-                  45%
+                  80%
                 </span>
               </div>
-              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-8 ">
-                <div className="bg-blue-600 h-2.5 rounded-full w-1/2"></div>
-              </div>
-
-
-
-              <div className="flex justify-between">
-                <span className="text-base font-medium text-blue-700 dark:text-white mb-4">
-                  Services
-                </span>
-                <span className="text-sm font-medium text-blue-700 dark:text-white">
-                  45%
-                </span>
-              </div>
-              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-8 ">
-                <div className="bg-blue-600 h-2.5 rounded-full w-1/2"></div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-2">
+                <div className="bg-blue-600 h-2.5 rounded-full w-10/12"></div>
               </div>
 
 
 
               <div className="flex justify-between">
-                <span className="text-base font-medium text-blue-700 dark:text-white mb-4">
-                  Services
+                <span className="text-base font-medium text-blue-700 dark:text-white mb-2">
+                  Amenities
                 </span>
                 <span className="text-sm font-medium text-blue-700 dark:text-white">
-                  45%
+                  70%
                 </span>
               </div>
-              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-8 ">
-                <div className="bg-blue-600 h-2.5 rounded-full w-1/2"></div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-2 ">
+                <div className="bg-blue-600 h-2.5 rounded-full w-8/12"></div>
+              </div>
+
+
+              
+              <div className="flex justify-between">
+                <span className="text-base font-medium text-blue-700 dark:text-white mb-2">
+                  Prices
+                </span>
+                <span className="text-sm font-medium text-blue-700 dark:text-white">
+                  60%
+                </span>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-2 ">
+                <div className="bg-blue-600 h-2.5 rounded-full w-8/12"></div>
               </div>
               
+
+
+              
+              <div className="flex justify-between">
+                <span className="text-base font-medium text-blue-700 dark:text-white mb-2">
+                  Food
+                </span>
+                <span className="text-sm font-medium text-blue-700 dark:text-white">
+                  75%
+                </span>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700  mb-2 ">
+                <div className="bg-blue-600 h-2.5 rounded-full w-3/4"></div>
+              </div>
 
               
             </div>
