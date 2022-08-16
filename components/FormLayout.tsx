c6fe425ae614d4776967c2cdecf5fe40a80f2705
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-16 16:06:36 +0300

    modified:   components/FormLayout.tsx

--- components/FormLayout.tsx
+++ components/FormLayout.tsx
@@ -14,97 +14,130 @@
   }
   ```
 */
-export default function FormLayout() {
-    return (
-      <>
-        <div className="max-w-7xl mx-auto" >
-            <div className="grid grid-col-2" >
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <form action="#" method="POST">
-                <div className="shadow sm:rounded-md sm:overflow-hidden">
-                  <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+
+import { StarIcon } from "@heroicons/react/solid";
+
+const reviews = { href: "#", average: 4, totalCount: 117 };
+
+type IFormLayoutProps = {
+  name?: string;
+  img?: string;
+  price?: string;
+  title?: string;
+  description?: string;
+  _id?: string;
+};
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+const activities = ["Wildlife", "Sight-seeing"];
+
+export default function FormLayout(props: IFormLayoutProps) {
+  return (
+    <>
+      <div className="">
+        <div className="flex ">
+          <div className="w-1/2">
+            <form action="#" method="POST">
+              <div className="shadow sm:rounded-md sm:overflow-hidden">
+                <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                  <div className="col-span-6 sm:col-span-3">
+                    <label
+                      htmlFor="first-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
 
                   <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                          First name
-                        </label>
-                        <input
-                          type="text"
-                          name="first-name"
-                          id="first-name"
-                          autoComplete="given-name"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-  
-                      <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                          Last name
-                        </label>
-                        <input
-                          type="text"
-                          name="last-name"
-                          id="last-name"
-                          autoComplete="family-name"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-  
-                      <div className="col-span-6 sm:col-span-4">
-                        <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                          Email address
-                        </label>
-                        <input
-                          type="text"
-                          name="email-address"
-                          id="email-address"
-                          autoComplete="email"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-                    
-  
-                    <div>
-                      <label htmlFor="about" className="block text-sm font-medium text-gray-700">
-                        Reviews
-                      </label>
-                      <div className="mt-1">
-                        <textarea
-                          id="about"
-                          name="about"
-                          rows={3}
-                          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                          placeholder="you@example.com"
-                          defaultValue={''}
-                        />
-                      </div>
-                      
+                    <label
+                      htmlFor="last-name"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div className="col-span-6 sm:col-span-4">
+                    <label
+                      htmlFor="email-address"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      name="email-address"
+                      id="email-address"
+                      autoComplete="email"
+                      className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Reviews
+                    </label>
+                    <div className="mt-1">
+                      <textarea
+                        id="about"
+                        name="about"
+                        rows={3}
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                        placeholder="you@example.com"
+                        defaultValue={""}
+                      />
                     </div>
-  
-  
                   </div>
                 </div>
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Submit
-                    </button>
-                  </div>
-              </form>
-            </div>
-
-            <div></div>
-
-            </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
           </div>
-    
-  
-  
-  
-  
-      </>
-    )
-  }
-  
+
+          <div className="flex w-1/2">
+            <p className="mr-80 font-bold text-md ml-20">Services</p>
+
+            {[0, 1, 2, 4, 5].map((rating) => (
+              <input
+                key={rating}
+                className={classNames(
+                  reviews.average > rating ? "text-gray-900" : "text-gray-200",
+                  "h-5 w-5 flex-shrink-0"
+                )}
+                type="checkbox"
+                aria-hidden="true"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
