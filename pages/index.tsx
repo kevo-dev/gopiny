0c35ec49f705bd49bb227c9fc774fbcc0b5c37b6
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2022-08-18 22:48:05 +0300

    modified:   components/layout/Section.tsx 	modified:   pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const AblyChatComponent = dynamic(() => import('../components/AblyChatComponent'
 const Home: NextPage = () => {
   return(
   <>
-    <div className="max-w-full ">
+    <div className="max-w-full gradient  ">
       
       <Navbar />
       <Carousel1 /> 
