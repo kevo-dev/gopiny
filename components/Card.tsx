5db60fe3ab71ccb247e7dd545ff856c2174da03b
_bro__code_ <97689028+bro-codez@users.noreply.github.com>
2023-01-19 18:12:08 +0300

    test2

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Section } from '../components/layout/Section'
+import { Section } from '../components/layout/Section';
 import Image from 'next/image';
 
 import Feature from '/svgs/feature.svg'
